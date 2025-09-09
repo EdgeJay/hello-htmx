339ee471786bf9c0c13f7a4221f59b9590b62d9c
EdgeJay <hjwu85@gmail.com>
2025-09-09 15:30:19 +0800

    create todo item, delete todo item (incomplete)

--- web/router.go
+++ web/router.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -23,6 +24,55 @@ func (r *Router) SetupRoutes() {
 		}
 
 		http.ServeFile(w, r, "./htmx/index.html")
+	})
+
+	// api
+	r.mux.HandleFunc("POST /api/todo", func(w http.ResponseWriter, r *http.Request) {
+		// read form value
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "Failed to parse form", http.StatusBadRequest)
+			return
+		}
+		todo := r.FormValue("todo")
+		if todo == "" {
+			http.Error(w, "Todo item cannot be empty", http.StatusBadRequest)
+			return
+		}
+
+		// In a real application, you would save the todo item to a database here.
+		// For this example, we'll just log it.
+		log.Printf("API: Adding todo item: %s\n", todo)
+
+		// read todo.html template file and replace {{todo}} with the actual todo item
+		tpl, err := template.ParseFiles("./htmx/todo.html")
+		if err != nil {
+			http.Error(w, "Failed to load template", http.StatusInternalServerError)
+			return
+		}
+
+		// execute template and write to response
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+
+		if err := tpl.Execute(w, map[string]string{"Todo": todo}); err != nil {
+			http.Error(w, "Failed to render template", http.StatusInternalServerError)
+			return
+		}
+	})
+
+	r.mux.HandleFunc("DELETE /api/todo/{id}", func(w http.ResponseWriter, r *http.Request) {
+		// read path value
+		id := r.PathValue("id")
+		if id == "" {
+			http.Error(w, "Todo ID cannot be empty", http.StatusBadRequest)
+			return
+		}
+
+		// In a real application, you would delete the todo item from a database here.
+		// For this example, we'll just log it.
+		log.Printf("API: Deleting todo item with ID: %s\n", id)
+
+		w.WriteHeader(http.StatusNoContent)
 	})
 
 	// static assets
