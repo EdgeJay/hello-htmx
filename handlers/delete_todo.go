08218ac6573a4e1411903e8bf3a42346afb42937
EdgeJay <hjwu85@gmail.com>
2025-09-13 23:51:22 +0800

    hook up delete functionality with frontend

--- handlers/delete_todo.go
+++ handlers/delete_todo.go
@@ -3,19 +3,33 @@ package handlers
 import (
 	"log"
 	"net/http"
+
+	mw "github.com/EdgeJay/hello-htmx/middlewares"
 )
 
 func DeleteTodo(w http.ResponseWriter, r *http.Request) {
 	// read path value
-	id := r.PathValue("id")
-	if id == "" {
+	todoId := r.PathValue("id")
+	if todoId == "" {
 		http.Error(w, "Todo ID cannot be empty", http.StatusBadRequest)
 		return
 	}
 
+	// get todo service from context
+	todoSvc := mw.GetTodoService(r)
+	if todoSvc == nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	// get session id
+	sessionID := mw.GetSessionID(r)
+
 	// In a real application, you would delete the todo item from a database here.
 	// For this example, we'll just log it.
-	log.Printf("API: Deleting todo item with ID: %s\n", id)
+	log.Printf("API: Deleting todo item with ID: %s\n", todoId)
 
-	w.WriteHeader(http.StatusNoContent)
+	todoSvc.DeleteTodo(sessionID, todoId)
+
+	w.WriteHeader(http.StatusOK)
 }
